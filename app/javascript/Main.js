b16757c8a6ba83e31c1077a3328d30a765478897
Nick Payne <slashnick@gmail.com>
2012-09-15 19:06:50 +0100

    Show and hide the keyboard based on the user
    Add key handling to search view so it can display the keyboard on focus
    and hide it when the user has entered their search term.
    
    Tidied up main.js so it's a self executing function and removed dom 1
    events from index.html.

--- app/javascript/Main.js
+++ app/javascript/Main.js
@@ -1,63 +1,16 @@
-var widgetAPI = new Common.API.Widget();
-var pluginAPI = new Common.API.Plugin();
-var tvKey = new Common.API.TVKeyValue();
+(function() {
 
-var Main = {};
+  "use strict";
 
-Main.onLoad = function() {
-	// Enable key event processing
-	new RT.SearchView();
-//	var searchUrl = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=[your_api_key]&q{searchTerm}",
-//	    model = new RT.SearchModel();
-//	new RT.SearchController(model, new RT.SearchView());
-//	new RT.MovieController(model, new RT.MovieView());
-	this.enableKeys();
-	pluginAPI.registIMEKey();
-	widgetAPI.sendReadyEvent();
-};
+  var widgetApi = new Common.API.Widget();
+  new RT.SearchView();
 
-Main.onUnload = function()
-{
+  // var searchUrl =
+  // "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=[your_api_key]&q{searchTerm}",
+  // model = new RT.SearchModel();
+  // new RT.SearchController(model, new RT.SearchView());
+  // new RT.MovieController(model, new RT.MovieView());
+  $("#keyHandler").focus();
+  widgetApi.sendReadyEvent();
 
-};
-
-Main.enableKeys = function()
-{
-	document.getElementById("anchor").focus();
-};
-
-Main.keyDown = function()
-{
-	var keyCode = event.keyCode;
-	alert("Key pressed: " + keyCode);
-
-	switch(keyCode)
-	{
-		case tvKey.KEY_RETURN:
-		case tvKey.KEY_PANEL_RETURN:
-			alert("RETURN");
-			widgetAPI.sendReturnEvent();
-			break;
-		case tvKey.KEY_LEFT:
-			alert("LEFT");
-			break;
-		case tvKey.KEY_RIGHT:
-			alert("RIGHT");
-			break;
-		case tvKey.KEY_UP:
-			alert("UP");
-			break;
-		case tvKey.KEY_DOWN:
-			alert("DOWN");
-			break;
-		case tvKey.KEY_ENTER:
-		case tvKey.KEY_PANEL_ENTER:
-			alert("ENTER");
-			break;
-		default:
-			alert("Unhandled key");
-			break;
-	}
-};
-
-Main.onLoad();
+}());
--- app/javascript/SearchView.js
+++ app/javascript/SearchView.js
@@ -1,17 +1,83 @@
 (function() {
+
+  "use strict";
+
+  /**
+   * A view to manage entering a search term
+   */
   function SearchView() {
     this._initialiseDom();
   }
 
+  /**
+   * The id of the input
+   * 
+   * @type {String}
+   */
   SearchView.prototype._INPUT_ID = "searchInput";
-  
+
+  /**
+   * The input element
+   * 
+   * @type {jQuery}
+   */
+  SearchView.prototype._jInput = null;
+
+  /**
+   * Initialise the dom for this view
+   */
   SearchView.prototype._initialiseDom = function() {
-    new IMEShell(this._INPUT_ID, this._imeReady.bind(this), "en");
+    this._jInput = $("#" + this._INPUT_ID);
+    new IMEShell(this._INPUT_ID, this._imeShellReady.bind(this), "en");
   };
-  
-  SearchView.prototype._imeReady = function() {
-    document.getElementById(this._INPUT_ID).focus();  
-    alert("READY");
+
+  /**
+   * Handle the input being ready
+   * 
+   * @param {IME} The input method editor
+   */
+  SearchView.prototype._imeShellReady = function(ime) {
+    var inputOffset = this._jInput.offset();
+    ime.setKeySetFunc("qwerty");
+    ime.setQWERTYPos(inputOffset.top + this._jInput.height(), inputOffset.left);
+    ime.setEnterFunc(this._performSearch.bind(this));
+
+    this._initialiseEvents();
+  };
+
+  /**
+   * Initialise the events for this view
+   */
+  SearchView.prototype._initialiseEvents = function() {
+    $("#keyHandler").bind("keydown", this._handleKeyDown.bind(this));
+  };
+
+  /**
+   * Handle a key being pressed
+   */
+  SearchView.prototype._handleKeyDown = function(event) {
+    var key = event.keyCode;
+    var tvKey = new Common.API.TVKeyValue();
+
+    switch (key) {
+      case tvKey.KEY_UP:
+      case tvKey.KEY_DOWN:
+      case tvKey.KEY_LEFT:
+      case tvKey.KEY_RIGHT:
+      case tvKey.KEY_RED:
+      case tvKey.KEY_ENTER:
+        this._jInput.focus();
+    }
+  };
+
+  /**
+   * Perform a search
+   * 
+   * @param {String} searchTerm The term to search for
+   */
+  SearchView.prototype._performSearch = function(searchTerm) {
+    alert("Searching for: " + searchTerm);
+    $("#keyHandler").focus();
   };
 
   RT.SearchView = SearchView;
